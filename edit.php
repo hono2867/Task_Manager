3dada59ed8a79e3a651a2ef0d325cb3c0d16d08c
hono2867 <22ue044@mb2.fwu.ac.jp>
2024-12-24 19:42:12 +0000

    Add baffa

--- edit.php
+++ edit.php
@@ -1,20 +1,29 @@
 <?php
+ob_start(); //出力バッファを開始
 require'header.php';
 require'Task.php';
 
 
 $task = Task::find($_GET['id']);
+$error = null;
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	Task::update($_GET['id'], $_POST['title'], $_POST['description']);
-	header('Location: index.php');
-	exit;
+	if (!$error) {
+		header('Location: index.php');
+		exit;
+	}
 }
+ob_end_flush(); //出力バッファをフラッシュ
 ?>
 
 
 <h2>Edit Task</h2>
+<?php if ($error): ?>
+    <p style="color: red;"><?php echo htmlspecialchars($error); ?></p>
+<?php endif; ?> //追加分
+
 <form method="post">
 	<label>Title</label>
 	<input type="text" name="title" value="<?php echo htmlspecialchars($task['title']); ?>" required>
